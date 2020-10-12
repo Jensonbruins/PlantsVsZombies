f6398625fdecc509a6df50c824cd889f8255e1be
Jenson <jenson@jensonbruins.nl>
2020-10-13 00:15:32 +0200

    Refactored modules.h

--- modules/modules.h
+++ modules/modules.h
@@ -4,12 +4,12 @@
 
 #ifndef PLANTSVSZOMBIES_MODULES_H
 #define PLANTSVSZOMBIES_MODULES_H
-#include "modules/entities/collision-detection/collision-detection.h"
-#include "modules/entities/plants/plants.h"
-#include "modules/entities/plants/input/input.h"
-#include "modules/entities/zombies/zombies.h"
-#include "modules/filesystem/filesystem.h"
-#include "modules/gfx/blitting/blitting.h"
-#include "modules/gfx/timing/timing.h"
-#include "modules/sfx/sfx.h"
+#include "entities/collision-detection/collision-detection.h"
+#include "entities/plants/plants.h"
+#include "entities/plants/input/input.h"
+#include "entities/zombies/zombies.h"
+#include "filesystem/filesystem.h"
+#include "gfx/blitting/blitting.h"
+#include "gfx/timing/timing.h"
+#include "sfx/sfx.h"
 #endif //PLANTSVSZOMBIES_MODULES_H
