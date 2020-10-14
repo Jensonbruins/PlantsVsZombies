b12a5c8d1fe1b91bdfb1a2f496628822b48f8c20
Jenson <jenson@jensonbruins.nl>
2020-10-14 11:28:20 +0200

    refactor(main): modify init statements

--- main.c
+++ main.c
@@ -13,18 +13,15 @@ int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Couldn't initialize SDL: %s\n", SDL_GetError());
         exit(1);
-    } window = SDL_CreateWindow("Blorp is going to F U UP!",
-                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                              SCREEN_HEIGHT, window_flags);
+    }
+    window = SDL_CreateWindow("PlantsVsZombies", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, window_flags);
     if (window == NULL) {
-        printf("Failed to create window -- Error: %s\n",
-               SDL_GetError());
+        printf("Failed to create window -- Error: %s\n", SDL_GetError());
         exit(1);
     }
     renderer = SDL_CreateRenderer(window, -1, renderer_flags);
     if (renderer == NULL) {
-        printf("Failed to create renderer -- Error: %s\n",
-               SDL_GetError());
+        printf("Failed to create renderer -- Error: %s\n", SDL_GetError());
         exit(1);
     }
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
@@ -36,16 +33,16 @@ int main(int argc, char *argv[]) {
     unsigned int firstFrame;
     unsigned int frameTime;
     while (1) {
-        firstFrame = SDL_GetTicks();
-        SDL_SetRenderDrawColor(renderer, 120, 144, 156, 255);
-        SDL_RenderClear(renderer);
+        firstFrame = SDL_GetTicks();                                    // Frame cap logic
+        SDL_SetRenderDrawColor(renderer, 120, 144, 156, 255);           // Background color
+        SDL_RenderClear(renderer);                                      // Remove all from renderer
 
-        process_input(window, renderer);
+        process_input(window, renderer);                                // Process key input and mouse input
 
-        SDL_RenderPresent(renderer);
-        frameTime = SDL_GetTicks() - firstFrame;
-        if (1000 / (unsigned int) Fps> frameTime) {
-            SDL_Delay(1000 / (unsigned int) Fps - frameTime);
+        SDL_RenderPresent(renderer);                                    // Create the big picture
+        frameTime = SDL_GetTicks() - firstFrame;                        // Frame cap logic
+        if (1000 / (unsigned int) Fps> frameTime) {                     // Frame cap logic
+            SDL_Delay(1000 / (unsigned int) Fps - frameTime);           // Frame cap logic
         }
     }
 }
