d32ac93e593c5438be2f5dfacd449c15e74ecb4f
Jenson <jenson@jensonbruins.nl>
2020-10-14 10:39:47 +0200

    feat(main): Add renderer and frame time logic

---
+++ includes/sdl.h
@@ -0,0 +1,14 @@
+//
+// Created by jenson on 14-10-20.
+//
+
+#ifndef PLANTSVSZOMBIES_SDL_H
+#define PLANTSVSZOMBIES_SDL_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h> // for IMG_Init and IMG_LoadTexture
+#include <SDL2/SDL_mixer.h>
+#define SCREEN_WIDTH				1600
+#define SCREEN_HEIGHT				900
+#endif //PLANTSVSZOMBIES_SDL_H
--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include "modules.h"
-
+#include "sdl.h"
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
 int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
+
+    unsigned int window_flags = 0;
+    unsigned int renderer_flags = SDL_RENDERER_ACCELERATED;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    } window = SDL_CreateWindow("Blorp is going to F U UP!",
+                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, window_flags);
+    if (window == NULL) {
+        printf("Failed to create window -- Error: %s\n",
+               SDL_GetError());
+        exit(1);
+    }
+    renderer = SDL_CreateRenderer(window, -1, renderer_flags);
+    if (renderer == NULL) {
+        printf("Failed to create renderer -- Error: %s\n",
+               SDL_GetError());
+        exit(1);
+    }
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
+        printf("Failed to create mixer -- Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    int Fps = 60;
+    unsigned int firstFrame;
+    unsigned int frameTime;
+    while (1) {
+        firstFrame = SDL_GetTicks();
+        SDL_SetRenderDrawColor(renderer, 120, 144, 156, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+        frameTime = SDL_GetTicks() - firstFrame;
+        if (1000 / (unsigned int) Fps> frameTime) {
+            SDL_Delay(1000 / (unsigned int) Fps - frameTime);
+        }
+    }
 }
