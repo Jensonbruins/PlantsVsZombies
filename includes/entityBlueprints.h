986dca9c90ca8d460923ba67b7eac0ae8d501c77
Jenson <jenson@jensonbruins.nl>
2020-10-14 15:31:10 +0200

    feat(zombie): add entity and zombie blueprint

---
+++ includes/entityBlueprints.h
@@ -0,0 +1,12 @@
+//
+// Created by jenson on 14-10-20.
+//
+
+#ifndef PLANTSVSZOMBIES_ENTITYBLUEPRINTS_H
+#define PLANTSVSZOMBIES_ENTITYBLUEPRINTS_H
+typedef struct _entity_ {
+    int x;
+    int y;
+    int health;
+} entity;
+#endif //PLANTSVSZOMBIES_ENTITYBLUEPRINTS_H
--- includes/zombieBlueprints.h
+++ includes/zombieBlueprints.h
@@ -4,5 +4,12 @@
 
 #ifndef PLANTSVSZOMBIES_ZOMBIEBLUEPRINTS_H
 #define PLANTSVSZOMBIES_ZOMBIEBLUEPRINTS_H
-
+#include "entityBlueprints.h"
+typedef struct _zombie_ {
+    entity components;
+    int priority;
+    SDL_Texture *textureWalk[];
+    SDL_Texture *textureAttack[];
+    SDL_Texture *textureDie[];
+} zombie;
 #endif //PLANTSVSZOMBIES_ZOMBIEBLUEPRINTS_H
