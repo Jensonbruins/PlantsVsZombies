c8a170f4a1c11486348081d712dff3a82a1c9725
Jenson <jenson@jensonbruins.nl>
2020-10-14 21:28:10 +0200

    feat(sun): add sun blueprint

--- includes/plantBlueprints.h
+++ includes/plantBlueprints.h
@@ -4,5 +4,13 @@
 
 #ifndef PLANTSVSZOMBIES_PLANTBLUEPRINTS_H
 #define PLANTSVSZOMBIES_PLANTBLUEPRINTS_H
+#include "entityBlueprints.h"
+#include "sdl.h"
+typedef struct _sun_ {
+    entity components;
+    int counter;
+    SDL_Texture *texture[2];
+} sun;
+
 
 #endif //PLANTSVSZOMBIES_PLANTBLUEPRINTS_H
