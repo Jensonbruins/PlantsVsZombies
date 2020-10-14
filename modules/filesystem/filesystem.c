d66713f93461016d3e84137ab5763ab2d2341ed7
Jenson <jenson@jensonbruins.nl>
2020-10-14 11:37:21 +0200

    feat(filesystem): add texture initializer and texture loader

--- modules/filesystem/filesystem.c
+++ modules/filesystem/filesystem.c
@@ -3,3 +3,20 @@
 //
 
 #include "filesystem.h"
+
+extern void texture_initializer(SDL_Renderer *renderer, char *path, char *filePrefix, int amountTextures, SDL_Texture *textureSet[]) {
+    for(int temp = 0; temp < amountTextures; temp++) {
+        char filename[50];
+
+        sprintf(filename, "%s%s%d.png", path, filePrefix, temp);
+        printf("Loading texture %s\n", filename);
+
+        textureSet[temp] = texture_loader(renderer, filename);
+    }
+}
+
+extern SDL_Texture *texture_loader(SDL_Renderer *renderer, char *filename) {
+    SDL_Texture *txtr;
+    txtr = IMG_LoadTexture(renderer, filename);
+    return txtr;
+}
--- modules/filesystem/filesystem.h
+++ modules/filesystem/filesystem.h
@@ -4,5 +4,9 @@
 
 #ifndef PLANTSVSZOMBIES_FILESYSTEM_H
 #define PLANTSVSZOMBIES_FILESYSTEM_H
+#include "sdl.h"
+
+extern void texture_initializer(SDL_Renderer *renderer, char *path, char *filePrefix, int amountTextures, SDL_Texture *textureSet[]);
+extern SDL_Texture *texture_loader(SDL_Renderer *renderer, char *filename);
 
 #endif //PLANTSVSZOMBIES_FILESYSTEM_H
