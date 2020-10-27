b90dac33eaceb78c49c65b8c900abd693cfc927d
Jenson <jenson@jensonbruins.nl>
2020-10-27 17:09:17 +0100

    fix: change collision detection to remove bug with plants not firing when 2 zombies are behind each other

--- modules/entities/collision-detection/collision-detection.c
+++ modules/entities/collision-detection/collision-detection.c
@@ -6,8 +6,6 @@
 
 static void zombie_collision_helper(zombie *zombieObject, lane *laneObject, plant plantArray[45]);
 
-static void in_range_helper(zombie *zombieObject, lane *laneObject, plant plantArray[45]);
-
 extern void zombie_check_collision(zombie zombieObject[50], int amount, lane laneArray[5], plant plantArray[45]) {
     for (int k = 0; k < amount; k++) {
         switch (zombieObject[k].lane) {
@@ -61,41 +59,24 @@ static void zombie_collision_helper(zombie *zombieObject, lane *laneObject, plan
 }
 
 extern void zombie_check_in_range(zombie zombieObject[50], int amount, lane laneArray[5], plant plantArray[45]) {
-    for (int k = 0; k < amount; k++) {
-        switch (zombieObject[k].lane) {
-            case 0:
-                in_range_helper(&zombieObject[k], &laneArray[0], plantArray);
-                break;
-            case 1:
-                in_range_helper(&zombieObject[k], &laneArray[1], plantArray);
-                break;
-            case 2:
-                in_range_helper(&zombieObject[k], &laneArray[2], plantArray);
-                break;
-            case 3:
-                in_range_helper(&zombieObject[k], &laneArray[3], plantArray);
-                break;
-            case 4:
-                in_range_helper(&zombieObject[k], &laneArray[4], plantArray);
-                break;
-            default:
-                continue;
-        }
-    }
-}
-
-static void in_range_helper(zombie *zombieObject, lane *laneObject, plant plantArray[45]) {
-    for (int k = 0; k < 9; k++) {
-        if (!(laneObject->blockArray[k].plantId > 45)) {
-            if (plantArray[laneObject->blockArray[k].plantId].canShoot == 1) {
-                if (plantArray[laneObject->blockArray[k].plantId].health > 0 && zombieObject->health > 0) {
-                    if (laneObject->blockArray[k].x < zombieObject->x && zombieObject->x < 1500) {
-                        plantArray[laneObject->blockArray[k].plantId].state = 1;
-                    } else {
-                        plantArray[laneObject->blockArray[k].plantId].state = 0;
+    for (int k = 0; k < 45; k++) {
+        if (plantArray[k].health > 0) {
+            if (plantArray[k].canShoot == 1) {
+                for (int v = 0; v < 5; v++) {
+                    for (int d = 0; d < 9; d++) {
+                        if (laneArray[v].blockArray[d].plantId == k) {
+                            for(int z = 0; z < amount; z++) {
+                                if (zombieObject[z].lane == v) {
+                                    if (laneArray[v].blockArray[d].x < zombieObject[z].x && zombieObject[z].x < 1500 && zombieObject[z].health > 0) {
+                                        plantArray[k].state = 1;
+                                        break;
+                                    } else {
+                                        plantArray[k].state = 0;
+                                    }
+                                }
+                            }
+                        }
                     }
-                } else {
-                    plantArray[laneObject->blockArray[k].plantId].state = 0;
                 }
             }
         }
